e5be900b05963923984a4a22edb1e767fcb63590
sayfutdiinov <105698172+Sayfudiinov-Samandar@users.noreply.github.com>
2022-12-25 12:47:05 +0500

    Added new slider

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() {
+    $('.slider').slick({
+        
+        // bu togmalr uchun
+        dots: true,
+
+        // default holatda nechta slider turishi
+        slidesToShow: 1,
+        
+        // bir next button ni bosganda nechta slider yurishi
+        slidesToScroll: 1,
+        
+        // tezligi
+        speed: 500,
+        
+        // slider lar qay tarizda harakat qilishi
+        easing: 'ease',
+        
+        // slider cheksiz ligi
+        infinite: true,
+        
+        // slider qaysi slidan boshlashi kerak ligini belgilash
+        initialSlide: 1,
+        
+        // sliderlar default ozgarib turish
+        autoplay: false,
+
+        //sliderlar default ozgarib turish vaqini belgilash
+        // autoplaySpeed: 500,
+
+        //silder hududiga bosganda tohtashi
+        // pouseOnFocus: true
+        //pouseOnHover: true
+        // pouseOnDotsHover: true
+
+
+        // mishka bilan click qilib sliderni surish ammo responsiv holatda yani tel va planshet larda ishlay
+        // draggable: false,
+
+        // responsive holatda scrol halt bilan otkazishni olib tashlash
+        // swipe: false
+        
+
+    });
+})
