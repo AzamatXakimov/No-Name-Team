b73633bd52ade36f782ef80afa0e3e4c58b4ebf6
sayfutdiinov <105698172+Sayfudiinov-Samandar@users.noreply.github.com>
2022-12-25 22:03:51 +0500

    added pease of footer

--- js/main.js
+++ js/main.js
@@ -175,3 +175,28 @@ elModalPasswordShowBtn.addEventListener("click", ()=> {
     }
 })
 
+
+
+// BTN HTML ELEEMENT TO TOP SCROLL
+
+const elBtn = document.querySelector("#myBtn")
+
+elBtn.addEventListener("click", topFunction)
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block"
+  } else {
+    document.getElementById("myBtn").style.display = "none"
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
